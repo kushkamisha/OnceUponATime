8bf3249f076d0605faedba0483bf73c60a13393b
Misha Kushka <kushka.misha@gmail.com>
2019-12-24 17:35:27 +0200

    Fix StartMenu scene

--- Assets/Script/Component/ExitOnClick.cs
+++ Assets/Script/Component/ExitOnClick.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class ExitOnClick : MonoBehaviour
 {
-    public void ExitGame()
-    {
-        Debug.Log("Exit");
-    }
+    public void ExitGame() { UnityEngine.Application.Quit(); }
 }
